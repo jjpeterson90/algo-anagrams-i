25b7e968128bb1e1a417f8eee308ed7fd0d8b720
Justin <jjpeterson90@hotmail.com>
2022-05-27 15:46:17 -0500

    complete

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,25 @@
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	string1_list = list(string1.casefold().replace(' ',''))
+	string2_list = list(string2.casefold().replace(' ',''))
+	
+	if len(string1_list) != len(string2_list):
+		return False
+	
+	for char in string1_list:
+		if char in string2_list:
+			string2_list.remove(char)
+	
+	if len(string2_list) != 0:
+		return False
+	else:
+		return True
+
+
+#print(is_character_match('charm', 'march')) # True
+#print(is_character_match('zach', 'attack')) # False
+#rint(is_character_match('CharM', 'mARcH')) # True
+#print(is_character_match('abcde2', 'c2abed')) # True
+
+#print(is_character_match('Anna Madrigal', 'A man and a girl')) # True
